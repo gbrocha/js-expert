9c8cfdcf14c65f17d99653d5afae69f2e6a95c29
Gabriel Rocha <gbrrochagbr@gmail.com>
2021-06-07 01:01:29 -0300

    preparing structure

---
+++ module_2/lesson05-tdd-project-pt02/test/unitTests/carService.test.js
@@ -0,0 +1,6 @@
+const { describe, it } = require('mocha')
+const CarService = require('../../src/service/carService')
+
+describe('CarService Suite Tests', () => {
+  it('give a carCategory it should return an available car', () => {})
+})
